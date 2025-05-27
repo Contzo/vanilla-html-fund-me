d2d2297bf5a4026bca7cf3b11194843575f6f5e5
Contzo <ilierazvan1497@outlook.com>
2025-05-27 15:59:31 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# What are we going to make?
+
+- Minimal HTML/JS site
+- That has the following buttons which map to the solidity smart contract.
+- Connect
+- Buy Cofee
+- Get Balance
+- Withdraw
---
+++ index-js.js
@@ -0,0 +1,43 @@
+import {
+  createWalletClient,
+  custom,
+  createPublicClient,
+} from "https://esm.sh/viem";
+
+const connectButton = document.getElementById("connectButton");
+const fundButton = document.getElementById("fundButton");
+const ethAmountInput = document.getElementById("ethAmount");
+
+let walletClient;
+let publicClient;
+async function connectWallet() {
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    }); // define a wallet client with a cusotom transport handled by MetaMask
+    const accounts = await walletClient.requestAddresses(); // request accounts for connection
+    connectButton.innerHTML = "Connected";
+  } else connectButton.innerHTML = "Please install MetaMask";
+}
+
+async function fund(event) {
+  event.preventDefault(); // prevent form default form submission
+  const ethAmount = ethAmountInput.value;
+  console.log(`Funding with ${ethAmount}`);
+  // check wallet connection
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    }); // define a wallet client with a cusotom transport handled by MetaMask
+    const accounts = await walletClient.requestAddresses(); // request accounts for connection
+    connectButton.innerHTML = "Connected";
+
+    publicClient = createPublicClient({
+      transport: custom(window.ethereum),
+    });
+  }
+}
+
+connectButton.onclick = connectWallet;
+
+fundButton.onclick = fund;
